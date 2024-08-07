b31fea88393979ef3de5a4e811637ceccd1e6087
Blake Farman <farmanb@gmail.com>
2024-08-07 09:57:50 -0500

    Added in the F24 script for ULM for posterity.

---
+++ F24.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Syllabi
+pretext build 1031-F24 && pretext build 1032-F24 && pretext build 2032-F24
+
+# Schedules
+
+pretext build schedule-1031-40997 && pretext build schedule-1032-40999 && pretext build schedule-2032-44879
