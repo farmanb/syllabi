e3e9cc902897abb1c9071705fc25d15379dc8ceb
Blake Farman <farmanb@gmail.com>
2024-09-06 14:41:23 -0500

    Fixed an issue with the schedule.

---
+++ F24.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Syllabi
+pretext build 243-F24
+
+# Schedules
+
+pretext build schedule-243-F24
--- S24.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Syllabi
-pretext build 1031-S24 && pretext build 3040-S24 && pretext build 63057 && pretext build 63167
-
-# Schedules
-
-pretext build schedule-63057 && pretext build schedule-63167 && pretext build schedule-1031-S24 && pretext build schedule-3040-S24
