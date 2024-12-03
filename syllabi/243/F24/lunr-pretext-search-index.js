1793824cb9e9742a0c14d7b901010a22b9227c6c
Blake Farman <farmanb@gmail.com>
2024-12-03 09:34:42 -0600

    Latest build deployed.

--- syllabi/243/F24/lunr-pretext-search-index.js
+++ syllabi/243/F24/lunr-pretext-search-index.js
@@ -1,22 +1,22 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "contact",
+  "id": "contact_information",
   "level": "1",
-  "url": "contact.html",
+  "url": "contact_information.html",
   "type": "Section",
   "number": "1",
   "title": "Contact Information",
   "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website      Office  Nethken 235       Office Hours     Mon\/Wed\/Fri  12:30 PM  3:00 PM    Tues\/Thurs  1:00 PM  2:15 PM       Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 243 - 004).    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. . X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 243 - 004  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
 },
 {
-  "id": "course_info",
+  "id": "course_information",
   "level": "1",
-  "url": "course_info.html",
+  "url": "course_information.html",
   "type": "Section",
   "number": "2",
   "title": "Course Information",
-  "body": " Course Information   Course Description  Vectors, three-dimensional coordinates, double and triple integrals, vector valued functions and motion in space.    Course Prerequisites  A grade of C or better in MATH 242.    Student Learning Outcomes  The student will become proficient in vectors and the geometry of space, vector functions, and multiple integration. This proficiency will be demonstrated by satisfactorily completing a series of exams and homework assignments.    Course Topics  Topics include   Vectors and the Geomtry of Space    Planes and Surfaces    Parametrization    Functions of Several Real Variables    Multiple Integration    Vector-Valued Functions       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+  "body": " Course Information   Course Description  Vectors, three-dimensional coordinates, double and triple integrals, vector valued functions and motion in space.    Course Prerequisites  A grade of C or better in MATH 242.    Student Learning Outcomes  The student will become proficient in vectors and the geometry of space, vector functions, and multiple integration. This proficiency will be demonstrated by satisfactorily completing a series of exams and homework assignments.    Course Topics  Topics include   Vectors and the Geometry of Space    Planes and Surfaces    Parameterization    Functions of Several Real Variables    Multiple Integration    Vector-Valued Functions       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
 },
 {
   "id": "evaluation",
@@ -25,12 +25,12 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "3",
   "title": "Evaluation",
-  "body": " Evaluation     Exams  There will be three in-class exams and a cumulative, common final exam. The exams are scheduled as follows:     Exam  Date    Exam 1  Thursday, September 29, 2024    Exam 2  Thursday, October 17, 2024    Exam 3  Thursday, November 7, 2024    Final Exam  November 13, 2024, 7 PM  9 PM , IESB 318          The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.        Homework  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the standards. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should ensure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.              Weights  Grades will be calculated using the weights below.     Grade Category  Weight    Homework  10%    Exam 1  20%    Exam 2  20%    Exam 3  20%    Final Exam  30%       Grading Scale  Letter grades will be assigned based on the scale below.     Average  Letter Grade     A     B     C     D     F      "
+  "body": " Evaluation     Exams  There will be three in-class exams and a cumulative, common final exam. The exams are scheduled as follows:     Exam  Date    Exam 1  Thursday, September 29, 2024    Exam 2  Thursday, October 17, 2024    Exam 3  Thursday, November 7, 2024    Final Exam  November 13, 2024, 7 PM  9 PM , IESB 318          The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.        Homework  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the standards. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should ensure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.    Weights  Grades will be calculated using the weights below.     Grade Category  Weight    Homework  10%    Exam 1  20%    Exam 2  20%    Exam 3  20%    Final Exam  30%       Grading Scale  Letter grades will be assigned based on the scale below.     Average  Letter Grade     A     B     C     D     F      "
 },
 {
-  "id": "evaluation-3-4",
+  "id": "exams-4",
   "level": "2",
-  "url": "evaluation.html#evaluation-3-4",
+  "url": "evaluation.html#exams-4",
   "type": "Warning",
   "number": "3.1",
   "title": "",
@@ -46,40 +46,40 @@ var ptx_lunr_docs = [
   "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook   Calculus: Early Transcendentals , Edition, by James Stewart, Daniel Clegg, Saleem Watson. ISBN : 1-337-61392-4.     Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of one missed exam due to absence, the Final Exam can be used to replace the missing exam. Further missed exams will result in a grade of zero. If a University sponsored activity requires you to miss an exam, you must complete the exam in advance.    Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
 },
 {
-  "id": "academic_serv",
+  "id": "academic_services",
   "level": "1",
-  "url": "academic_serv.html",
+  "url": "academic_services.html",
   "type": "Section",
   "number": "5",
   "title": "Academic Services",
   "body": " Academic Services   BARC  Free tutoring sessions held by the BARC , Wyly Tower room 225, are open to anyone on a walk-in or Zoom-In basis. Sessions will begin the second week of classes. Hours can be found on their website .    Math Club  The math club hosts free, weekly Math & Music tutoring sessions. The math club also maintains a list of private tutors ; please contact them for more information. More information can be found on the Math Club website .   "
 },
 {
-  "id": "academic_serv-2-2",
+  "id": "BARC-2",
   "level": "2",
-  "url": "academic_serv.html#academic_serv-2-2",
+  "url": "academic_services.html#BARC-2",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "BARC "
 },
 {
-  "id": "academic_serv-3-2",
+  "id": "math_club-2",
   "level": "2",
-  "url": "academic_serv.html#academic_serv-3-2",
+  "url": "academic_services.html#math_club-2",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "Math & Music private tutors "
 },
 {
-  "id": "student_serv",
+  "id": "student_services",
   "level": "1",
-  "url": "student_serv.html",
+  "url": "student_services.html",
   "type": "Section",
   "number": "6",
   "title": "Student Services",
-  "body": " Student Services   Disability Accommodations  Students needing testing or classroom accommodations based on a disability are encouraged to discuss those needs with me as soon as possible. Please see Office of Disability Services website for more information.    Counseling  Information, contact numbers, and sites for Louisiana Tech Counseling Services are located on their website .    Title IX and Discrimination Concerns  Louisiana Tech University adheres to the equal opportunity provisions of federal and civil rights laws, and does not discriminate on the basis of race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability.      Director of Title IX Compliance, President’s Office  Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.          Section 504 Coordinator  Annie Jantz    Phone:  318.257.2445    Office:  Keeny Hall 305    Email:  ajantz@latech.edu                    Emergency Notification System  Emergency Notification System: All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency. For more information visit the Emergency Notification System page.   "
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.     Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Drop with a W date  The latest date a student may drop a course (or resign his\/her entire schedule) with a W grade for Winter 2024 25 is Friday, February 7, 2025.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
 },
 {
   "id": "schedule",
