b9dc89d76a9a39319a51c8edde6f558bd142d06e
Blake Farman <farmanb@gmail.com>
2025-01-04 09:56:41 -0600

    Updated script to include 587

--- S25.sh
+++ S25.sh
@@ -3,7 +3,10 @@
 # Syllabi
 pretext build 490-S25
 pretext build 490-S25-print
+pretext build 587-S25
+pretext build 587-S25-print
 
 # Schedules
 
 pretext build schedule-490-S25
+pretext build schedule-587-S25
