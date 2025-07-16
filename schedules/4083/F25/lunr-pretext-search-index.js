42a14ca9b423bd96932f1bf2bba7d7afa01a6ec7
blake-farman <blake.farman@gmail.com>
2025-07-16 12:32:15 -0500

    Latest build deployed.

---
+++ schedules/4083/F25/lunr-pretext-search-index.js
@@ -0,0 +1,32 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Schedule",
+  "body": " Schedule             September 4 September 5     September 5   1 Binary Operations  Associativity,  Identity,  Inverses,  Commutativity.         September 8 September 12     September 8   2 Groups  Axioms,  Homomorphisms,  Elementary Properties      September 10   3 Abelian Examples  Numerical Groups  Integers Modulo  Roots of Unity      September 12   4 Nonabelian Examples  Symmetric Groups  Dihedral Groups         September 15 September 19     September 15   5 Subgroups  Definition  Subgroup Criterion  Cyclic and Subgroups      September 17   6 Cyclic Groups  Division Algorithm  Greatest Common Divisors  Classification      September 19   8 Groups of Permutations  Properties of Homomorphisms   Subgroups Associated to Homomorphisms:  Images,  Inverse Images,  Kernels     Cayley's Theorem         September 22 September 26     September 22   9 Finitely Generated Abelian Groups  Products and Coproducts  Fundamental Theorem of Finitely Generated Abelian Groups      September 24   10 Cosets and the Theorem of Lagrange  Left\/Right Equivalence Modulo a Subgroup  Left and Right Cosets  Lagrange's Theorem  Index of a Subgroup  Left\/Right Cosets and Kernels      September 26   12 Factor Groups  Normal Subgroups  Quotients  Universal Mapping Property for Quotients  First Isomorphism Theorem for Groups  Inner Automorphisms         September 29 October 3     September 29   13 Factor-Group Computations and Simple Groups  Examples  Centers  Commutators  Simple Groups      October 1  Review    October 3   Exam 1        October 6 October 10     October 6   22 Rings and Fields  Rng Axioms  Ring Axioms  Examples      October 8   22 Rings and Fields  Homomorphisms  Fields      October 10   23 Integral Domains  Definition  Zero Divisors  Cancellation  Characteristic         October 13 October 17     October 13   27 Rings of Polynomials  Definitions  Basic Properties      October 15   27 Rings of Polynomials  Roots of Polynomials  Evaluation Homomorphism for Fields      October 17   28 Factorization of Polynomials over a Field  Division Algorithm  Factoring  Irreducibility         October 20 October 24     October 20   28 Factorization of Polynomials over a Field  Eisenstein's Criterion  Uniqueness of Factorization      October 22  Review    October 24  Exam 2       October 27 October 31     October 27   30 Homomorphisms and Factor Rings  Ideals  Quotient Rings  Universal Mapping Property for Quotients (Reprise)  First Isomorphism Theorem for Rings      October 29   31 Prime and Maximal Ideals  Prime Ideals  Maximal Ideals      October 31   31 Prime and Maximal Ideals  Applications to Polynomials Rings over a Field  Applications to Factoring         November 3 November 7     November 3   34 Unique Factorization Domains    November 5   34 Unique Factorization Domains    November 7   37 Algebraic Geometry  Algebraic Varieties  Examples         November 10 November 13     November 10  Review    November 12   Final Exam               The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "1.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
---
+++ syllabi/4083/F25/lunr-pretext-search-index.js
@@ -0,0 +1,95 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright   "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website      Office  Nethken 235       Office Hours    Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject    Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. . X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  Fundamental set concepts, groups, rings, integral domains, fields, polynomials.    Course Prerequisites  A grade of C or better in MATH 3073 (Previously MATH 307) or MATH 3113 (Previously MATH 311).    Student Learning Outcomes  The primary goal of this course is to gain familiarity with basic algebraic structures (groups, rings, and fields) and how they interact with one another (actions, homomorphisms, etc.). An equally important goal of this course is to improve students’ ability to read and write mathematical proofs.    Course Topics  This course can be divided into two parts. In the first, we cover the definitions and examples of groups, subgroups, quotient groups, homomorphisms, and important theorems about the structure of certain groups. In the second, we encounter basic definitions and examples of rings, subrings, ideals, quotients of rings, domains, fields, homomorphisms, and other topics as time permits.    Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation     Exams  There will be two in-class exams and a cumulative final exam. The exams are scheduled as follows:     Exam  Date    Exam 1  Friday, September 26, 2025    Exam 2  Monday, October 20, 2025    Final Exam  Wednesday, November 12, 2025         The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.        Homework  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the material. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should ensure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.    Weights  Grades will be calculated using the weights below.   Evaluation Criteria    Grade Category  Weight    Homework  50%    Exams  20%    Final Exam  30%       Grading Scale  Letter grades will be assigned based on the scale below.   Grading Scale    Average  Letter Grade     A     B     C     D     F      "
+},
+{
+  "id": "exams-4",
+  "level": "2",
+  "url": "evaluation.html#exams-4",
+  "type": "Warning",
+  "number": "3.1",
+  "title": "",
+  "body": "    The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook   A First Course in Abstract Algebra , Eighth edition, by John B. Fraleigh and Neal Brand, ISBN 9780136731627.     Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of one missed exam due to absence, the Final Exam can be used to replace the missing exam. Further missed exams will result in a grade of zero. If a University sponsored activity requires you to miss an exam, you must complete the exam in advance.    Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.     Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).   "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Schedule",
+  "body": " Schedule             September 4 September 5     September 5   1 Binary Operations  Associativity,  Identity,  Inverses,  Commutativity.         September 8 September 12     September 8   2 Groups  Axioms,  Homomorphisms,  Elementary Properties      September 10   3 Abelian Examples  Numerical Groups  Integers Modulo  Roots of Unity      September 12   4 Nonabelian Examples  Symmetric Groups  Dihedral Groups         September 15 September 19     September 15   5 Subgroups  Definition  Subgroup Criterion  Cyclic and Subgroups      September 17   6 Cyclic Groups  Division Algorithm  Greatest Common Divisors  Classification      September 19   8 Groups of Permutations  Properties of Homomorphisms   Subgroups Associated to Homomorphisms:  Images,  Inverse Images,  Kernels     Cayley's Theorem         September 22 September 26     September 22   9 Finitely Generated Abelian Groups  Products and Coproducts  Fundamental Theorem of Finitely Generated Abelian Groups      September 24   10 Cosets and the Theorem of Lagrange  Left\/Right Equivalence Modulo a Subgroup  Left and Right Cosets  Lagrange's Theorem  Index of a Subgroup  Left\/Right Cosets and Kernels      September 26   12 Factor Groups  Normal Subgroups  Quotients  Universal Mapping Property for Quotients  First Isomorphism Theorem for Groups  Inner Automorphisms         September 29 October 3     September 29   13 Factor-Group Computations and Simple Groups  Examples  Centers  Commutators  Simple Groups      October 1  Review    October 3   Exam 1        October 6 October 10     October 6   22 Rings and Fields  Rng Axioms  Ring Axioms  Examples      October 8   22 Rings and Fields  Homomorphisms  Fields      October 10   23 Integral Domains  Definition  Zero Divisors  Cancellation  Characteristic         October 13 October 17     October 13   27 Rings of Polynomials  Definitions  Basic Properties      October 15   27 Rings of Polynomials  Roots of Polynomials  Evaluation Homomorphism for Fields      October 17   28 Factorization of Polynomials over a Field  Division Algorithm  Factoring  Irreducibility         October 20 October 24     October 20   28 Factorization of Polynomials over a Field  Eisenstein's Criterion  Uniqueness of Factorization      October 22  Review    October 24  Exam 2       October 27 October 31     October 27   30 Homomorphisms and Factor Rings  Ideals  Quotient Rings  Universal Mapping Property for Quotients (Reprise)  First Isomorphism Theorem for Rings      October 29   31 Prime and Maximal Ideals  Prime Ideals  Maximal Ideals      October 31   31 Prime and Maximal Ideals  Applications to Polynomials Rings over a Field  Applications to Factoring         November 3 November 7     November 3   34 Unique Factorization Domains    November 5   34 Unique Factorization Domains    November 7   37 Algebraic Geometry  Algebraic Varieties  Examples         November 10 November 13     November 10  Review    November 12   Final Exam               The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "6.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
